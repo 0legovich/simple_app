bbff1567d1523bae6f90185b2dd4fbff81a92dc7
Roman <romq1@mail.ru>
2017-02-13 07:30:39 -0800

    Finish layout and routers

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,48 +1,29 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject {page}
 	let (:titule) { "Страница" }
+
   describe "Home page" do
-    it "should have the content 'Simple App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    it "Проверяем динамический заголовок Домашняя" do
-  		visit '/static_pages/home'
-  		expect(page).to have_title("#{titule}")
-  	end 
+    before {visit root_path}
+    it {should have_content('Sample App')}
+    it {should have_title("#{titule}")}
   end
 
   describe "Help page" do
-  	it "должно быть слово 'Помощь'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Помощь')
-  	end
-  	it "Проверяем динамический заголовок Помощь" do
-  		visit '/static_pages/help'
-  		expect(page).to have_title("#{titule} Помощь")
-  	end 
+    before {visit help_path}
+  	it {should have_content('Помощь')}
+  	it {have_title("#{titule} Помощь")}
   end
 
   describe "Страница 'О нас'" do
-  	it "Проверяем страницу на слово 'О нас'" do
-  	visit '/static_pages/about'
-  	expect(page).to have_content('О нас')
-  	end
-  	it "Проверяем динамический заголовок О нас" do
-  		visit '/static_pages/about'
-  		expect(page).to have_title("#{titule} О нас")
-  	end 
+    before {visit about_path}
+  	it {have_content('О нас')}
+  	it {have_title("#{titule} О нас")}
   end
   describe "Страница 'Контакты'" do
-  	it "Есть ли страница 'Контакты'" do
-  		visit '/static_pages/contact'
-  		expect(page).to have_content ('Контакты')
-  	end
-  	it "Проверяем динамический заголовок 'Контакты'" do
-  		visit '/static_pages/contact'
-  		expect(page).to have_title("#{titule} Контакты")
-  	end
+    before {visit contact_path}
+  	it {have_content ('Контакты')}
+  	it {have_title("#{titule} Контакты")}
   end
 end
